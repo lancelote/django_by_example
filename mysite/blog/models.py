2041e85109a84974f317a5bc4856bf71d3945622
Pavel Karateev <lancelote.du.lac@gmail.com>
2016-02-16 17:43:17 +0300

    Fix default date/time for Post publish field

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, related_name='blog_posts')
     body = models.TextField()
-    publish = models.DateTimeField(default=timezone.now())
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICE, default='draft')
