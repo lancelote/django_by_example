74d613d4772c770181ad084b766046812042a9f3
Pavel Karateev <lancelote.du.lac@gmail.com>
2016-02-16 17:38:43 +0300

    blog to installed apps, 120 chars line length

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -16,11 +16,7 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now())
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-        max_length=10,
-        choices=STATUS_CHOICE,
-        default='draft'
-    )
+    status = models.CharField(max_length=10, choices=STATUS_CHOICE, default='draft')
 
     class Meta:
         ordering = ('-publish',)
