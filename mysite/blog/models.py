25a8d6a2b95f0062f5da0ffed5de4bf4d8fa0cad
Pavel Karateev <lancelote.du.lac@gmail.com>
2016-02-16 17:34:54 +0300

    Blog app and Post model, pytz to requirements

---
+++ mysite/blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+
+    STATUS_CHOICE = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now())
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICE,
+        default='draft'
+    )
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
