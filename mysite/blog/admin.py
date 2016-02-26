fef365e24d76bc0fe1675c6ff58fb614472e330e
Pavel Karateev <lancelote.du.lac@gmail.com>
2016-02-26 14:30:27 +0300

    Customises admin panel

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,22 @@
+# pylint: disable=missing-docstring
+
+"""
+Mysite admin panel
+"""
+
 from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'slug', 'author', 'publish', 'status',)
+    list_filter = ('status', 'created', 'publish', 'author',)
+    search_fields = ('title', 'body',)
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+admin.site.register(Post, PostAdmin)
